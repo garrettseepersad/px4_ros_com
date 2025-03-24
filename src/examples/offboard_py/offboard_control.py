e989a773fe3ede3c1f7701ce1b63ff9728f48f60
Garrett Seepersad <35423760+garrettseepersad@users.noreply.github.com>
2025-03-24 11:20:25 +0000

    Fix python example to subscribe to the correct message
    Signed-off-by: Garrett Seepersad <35423760+garrettseepersad@users.noreply.github.com>

--- src/examples/offboard_py/offboard_control.py
+++ src/examples/offboard_py/offboard_control.py
@@ -32,7 +32,7 @@ class OffboardControl(Node):
         self.vehicle_local_position_subscriber = self.create_subscription(
             VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
         self.vehicle_status_subscriber = self.create_subscription(
-            VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile)
+            VehicleStatus, '/fmu/out/vehicle_status_v1', self.vehicle_status_callback, qos_profile)
 
         # Initialize variables
         self.offboard_setpoint_counter = 0
